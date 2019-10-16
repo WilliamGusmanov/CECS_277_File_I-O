63b6b95fe1063054ee5ae03349efc862e83162e1
William Gusmanov <Williamgusmanov1@gmail.com>
2019-10-16 14:25:54 -0700

    update

--- EnterSales.java
+++ EnterSales.java
@@ -4,6 +4,17 @@ import java.io.*;
 import java.util.Scanner; 
 import javax.swing.*; 
 public class EnterSales {
+	//TO DO LIST:
+	/*
+	
+	- [ ] Write your own exception class for any errors that might come up in the
+	sales records, such as an invalid service name, invalid date or invalid
+	price.
+	- [ ] Part 2 Output to files
+	- [ ] Validations 
+	
+	*/
+	
 	
 	ArrayList<Service> ServicesList = new ArrayList<Service>();
 	
@@ -27,13 +38,16 @@ public class EnterSales {
 	 * read list from file 
 	 * @param validFile
 	 */
-	public void readServicesFromFile(File validFile) {
+	public void readServicesFromFileintoNewFiles(File validFile) {
 		try {
 			Scanner textFile = new Scanner(validFile);
-			String customerName = textFile.next();
-			String serviceName = textFile.next();
-			float Price = textFile.nextFloat();
-			String Date = textFile.nextLine();
+			ArrayList<String> list = new ArrayList<String>();
+			while (textFile.hasNext()) { //for every line in the textfile
+				String input = textFile.nextLine();
+				String[] splitInput = input.split(";");
+				//validate input here * * * * * * 
+				
+			}
 			
 			System.out.println("Regular File Found!");	 
 			textFile.close();
@@ -61,7 +75,6 @@ public class EnterSales {
 						System.out.println("Enter name of customer");
 						ServicesList.get(i).setNameOfCustomer(console.nextLine()); //Set name of customer 
 						System.out.println("Enter name of service ex.) Breakfast, Lunch, Dinner, Conference, Tea, Massage");
-						
 						ServicesList.get(i).setNameOfService(console.next()); //set name of service 
 						System.out.println("Enter price of service: ");
 						ServicesList.get(i).setPriceOfService(console.nextFloat()); 
@@ -77,24 +90,29 @@ public class EnterSales {
 						}
 						console.nextLine(); //burn nextline 
 					}
+					BufferedWriter writer = new BufferedWriter(fileWriter);
 					for (i = 0; i < ServicesList.size(); i++) {
 						Service service  = ServicesList.get(i);
-						String serviceOutput = service.getNameOfCustomer()+", "+service.getNameOfService()+", "+
-						service.getPriceOfService()+", "+service.getDateAsString(); 
-						fileWriter.write(serviceOutput);
-						fileWriter.flush();
-					}
+						String serviceOutput = service.getNameOfCustomer()+";"+service.getNameOfService()+";"+
+						service.getPriceOfService()+";"+service.getDateAsString(); 
+						writer.write(serviceOutput);
+						writer.newLine();
+						writer.flush();
+						} //end of for loop 
+					writer.close(); 
 					fileWriter.close();
 					console.close();
-				} 
+				} // end of try  
 				catch(IOException e) {
 					System.out.println(e.getMessage());
-				}
+				} //end of catch 1
 				catch (FileNotFoundException e) {
 					System.out.println("Invalid File!");
-				} //end of catch 
-			}//end of while loop
-		//create a service 
-		//add it to the textFile
-		}
-	}
+				} //end of catch 2
+				catch (java.io.IOException e) {
+					System.out.println("Error with Buffer reader");
+					e.printStackTrace();
+				} //end of catch 3
+			} //end of while loop
+		}//end of function definition 
+	} //end of class 
