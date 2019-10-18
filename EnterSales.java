26780d40d93b874bd777701026f948972904e9f0
William Gusmanov <40575394+WilliamGusmanov@users.noreply.github.com>
2019-10-17 20:46:41 -0700

    Update EnterSales.java

--- EnterSales.java
+++ EnterSales.java
@@ -1,19 +1,18 @@
 package fileInputOutPackage;
 import java.util.ArrayList;
 import java.io.*;
+import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner; 
 import javax.swing.*;
 
 public class EnterSales {
-	
 	/**
 	 * the main that runs the EnterSales Application
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		//File inputFile = inputFile();
 		EnterSales Enter = new EnterSales();
 		File inputFile = Enter.inputFile(); 
 		Enter.addAllServicesToFile(inputFile);
@@ -27,8 +26,8 @@ public class EnterSales {
 		for (Service x : ServicesList) {
 			System.out.println(x.getNameOfCustomer()+", "+x.getNameOfService()+", "+
 					x.getPriceOfService()+", "+x.getDateAsString());
-		}
-	}
+		} //end for loop
+	}//end function definition
 	/**
 	 * asks user for the input file to write the service elements to
 	 * @return
@@ -36,17 +35,16 @@ public class EnterSales {
 	public File inputFile() {
 	JFileChooser jFileChooser = new JFileChooser(); 
 		if (jFileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
-			//assume sales.txt exists already in file directory
 			String outname = jFileChooser.getSelectedFile().getAbsolutePath();
 			File validFile = new File (outname); //input file path as chosen using JFileChooser 
 			return validFile; 
 		}
 		else return null; 
 	}
-	
 	/**
 	 * Write services into the text file 
-	 * @param validFile
+	 * prompts user to enter Customer name, the service, price, and date the service will be given  
+	 * @param validFile, the file the user chose to input to
 	 */
 	public void addAllServicesToFile(File validFile) {
 		ArrayList<Service> ServicesList = new ArrayList<Service>();
@@ -62,9 +60,9 @@ public class EnterSales {
 					while (continueInput == 'y') {
 						ServicesList.add(new Service()); 
 						System.out.println("Enter name of customer");
-						ServicesList.get(i).setNameOfCustomer(console.nextLine()); //Set name of customer 
+						ServicesList.get(i).setNameOfCustomer(console.nextLine());
 						System.out.println("Enter name of service ex.) Breakfast, Lunch, Dinner, Conference, Tea, Massage");
-						ServicesList.get(i).setNameOfService(console.next()); //set name of service 
+						ServicesList.get(i).setNameOfService(console.next()); 
 						System.out.println("Enter price of service: ");
 						ServicesList.get(i).setPriceOfService(console.nextFloat()); 
 						System.out.println("Enter date of service MM/dd/uuuu: ");
@@ -95,78 +93,91 @@ public class EnterSales {
 				catch(IOException e) {
 					System.out.println(e.getMessage());
 				} //end of catch 1
+				catch(IllegalArgumentException e) {
+					System.out.println("Enter a valid service.");
+				}
+				catch (InvalidFloatException e) {
+					System.out.print(e.getMessage());
+				}
+				catch (DateTimeException e) {
+					System.out.println("Invalid date, please enter a valid date.");
+					}
 			} //end of while loop
 		}//end of function definition 
+	
 	/**
 	 * Nested class Service
 	 * which contains enum of possible services, associated customer, and the float price of the service
 	 * 
 	 */
-	public static class Service {
-		enum nameOfService{
-			Breakfast,Lunch,Dinner,Conference,Tea,Massage
-		}
-		private String nameOfCustomer;
-		private nameOfService serviceName; //Breakfast, lunch, dinner, etc...
-		private float priceOfService;
-		
-		LocalDate date;
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/uuuu"); 
-		private String dateAsString;
-		
-		public void inputDate(String inputDate){
-			String[] input = inputDate.split("/");
-			int month = Integer.parseInt(input[0]);
-			int day = Integer.parseInt(input[1]);
-			int year = Integer.parseInt(input[2]);
-			date = LocalDate.of(year, month, day); 
-			dateAsString = date.format(formatter);	
-		}
-		//default constructor
-		public Service() {
-			this.nameOfCustomer = "Customer";
-			this.serviceName = nameOfService.Breakfast;
-			this.priceOfService = 0.0f;
-			this.date = LocalDate.now();
-			this.dateAsString = this.date.format(formatter); 
+		public static class Service {
+			enum nameOfService{
+				Breakfast,Lunch,Dinner,Conference,Tea,Massage
+			}
+			private String nameOfCustomer;
+			private nameOfService serviceName; //Breakfast, lunch, dinner, etc...
+			private float priceOfService;
 			
-		}
-		//second constructor
-		public Service(String nameOfCustomer, String serviceName, float priceOfService, LocalDate date) {
-			this.nameOfCustomer = nameOfCustomer;
-			this.serviceName = nameOfService.valueOf(serviceName);
-			this.priceOfService = priceOfService;
-			this.date = date;
-		}
-		public String getNameOfCustomer() {
-			return nameOfCustomer;
-		}
-		public void setNameOfCustomer(String nameOfCustomer) {
-			this.nameOfCustomer = nameOfCustomer;
-		}
-		public nameOfService getNameOfService() {
-			return serviceName;
-		}
-		public void setNameOfService(String serviceName) {
-			this.serviceName = nameOfService.valueOf(serviceName);
-		}
-		public float getPriceOfService() {
-			return priceOfService;
-		}
-		public void setPriceOfService(float priceOfService) {
-			this.priceOfService = priceOfService;
-		}
-		public LocalDate getDate() {
-			return date;
-		}
-		public String getDateAsString() {
-			return dateAsString;
-		}
-		@Override
-		public String toString() {
-			return "Person:" + nameOfCustomer + "Service: " + serviceName + ", price: " + priceOfService + ", date: "
-					+ dateAsString + "]";
-		}
-	} //end of service class
-
+			LocalDate date;
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/uuuu"); 
+			private String dateAsString;
+			/**
+			 * Takes the date as a full string and sets the date as a string in the correct format
+			 * Throws exception if incorrect format inside of formatter
+			 * @param inputDate
+			 */
+			public void inputDate(String inputDate) throws java.time.DateTimeException{
+				String[] input = inputDate.split("/");
+				int month = Integer.parseInt(input[0]);
+				int day = Integer.parseInt(input[1]);
+				int year = Integer.parseInt(input[2]);
+				date = LocalDate.of(year, month, day); 
+				dateAsString = date.format(formatter);	
+			} 
+			//default constructor
+			public Service() {
+				this.nameOfCustomer = "Customer";
+				this.serviceName = nameOfService.Breakfast;
+				this.priceOfService = 0.0f;
+				this.date = LocalDate.now();
+				this.dateAsString = this.date.format(formatter); 
+				
+			}
+			//second constructor
+			public Service(String nameOfCustomer, String serviceName, float priceOfService, LocalDate date) {
+				this.nameOfCustomer = nameOfCustomer;
+				this.serviceName = nameOfService.valueOf(serviceName);
+				this.priceOfService = priceOfService;
+				this.date = date;
+			}
+			public String getNameOfCustomer() {
+				return nameOfCustomer;
+			}
+			public void setNameOfCustomer(String nameOfCustomer) {
+				this.nameOfCustomer = nameOfCustomer;
+			}
+			public nameOfService getNameOfService() {
+				return serviceName;
+			}
+			public void setNameOfService(String serviceName) throws IllegalArgumentException {
+				this.serviceName = nameOfService.valueOf(serviceName);
+			}
+			public float getPriceOfService() {
+				return priceOfService;
+			}
+			public void setPriceOfService(float priceOfService) {
+				this.priceOfService = priceOfService;
+			}
+			public LocalDate getDate() {
+				return date;
+			}
+			public String getDateAsString() {
+				return dateAsString;
+			}
+			@Override
+			public String toString() {
+				return "Person:" + nameOfCustomer + "Service: " + serviceName + ", price: " + priceOfService + ", date: "
+						+ dateAsString + "]";
+			}
+		} //end of service class
 	} //end of class 
